619f103b60ea922f7a45f18499b3c6eccf170208
Felipe Portela <fc.portela@icluoud.com>
2024-07-29 22:33:51 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.log(“Rodando o sitema de gerenciamento de jogos”) 
+console.log(“Rodando o sistema de gerenciamento de jogos”)
+conseole.log(“aplçicaçao esta se conectando ao banco de dados”)
